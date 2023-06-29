35727994cb8a85da2b7de76ec4279e8b05fff56b
Alan <alan@alanlin.io>
2023-06-29 09:30:41 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+import json
+import random
+import string
+from threading import Timer
+
+app = Flask(__name__)
+
+# The password that allows access to the name submission form
+password = ''.join(random.choice(string.ascii_lowercase) for i in range(6))
+
+def update_password():
+    global password
+    password = ''.join(random.choice(string.ascii_lowercase) for i in range(6))
+    Timer(60, update_password).start()
+
+update_password()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if request.form['password'] == password:
+            with open('list.json', 'r+') as f:
+                data = json.load(f)
+                data.append(request.form['name'])
+                f.seek(0)
+                json.dump(data, f)
+            return redirect(url_for('member'))
+    return render_template('index.html')
+
+@app.route('/securepasspage')
+def securepasspage():
+    return jsonify({'password': password})
+
+@app.route('/member')
+def member():
+    with open('list.json', 'r') as f:
+        data = json.load(f)
+    return render_template('member.html', data=data)
+
+@app.route('/secureadmin', methods=['GET', 'POST'])
+def secureadmin():
+    if request.method == 'POST':
+        if request.form['action'] == 'randomize':
+            with open('list.json', 'r+') as f:
+                data = json.load(f)
+                random.shuffle(data)
+                f.seek(0)
+                json.dump(data, f)
+        elif request.form['action'] == 'clear':
+            with open('list.json', 'w') as f:
+                json.dump([], f)
+    with open('list.json', 'r') as f:
+        data = json.load(f)
+    return render_template('secureadmin.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
