f4cbd2ffaeab0929943e12a6e95e37cc2584bd58
Alan Lin <alanlin@Alans-MacBook-Air.local>
2023-08-01 14:44:56 -0700

    fix4

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def index():
 @app.route('/securepasspage')
 def securepasspage():
     global password
-    return jsonify({'password': password})
+    return render_template('securepasspage.html', password=password)
 
 @app.route('/member')
 def member():
